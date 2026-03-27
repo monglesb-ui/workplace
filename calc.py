424f1c57110e6635ecc907fb68dae986003b4030
monglesb-ui <monglesb@gmail.com>
2026-03-27 11:08:01 +0900

    Add files via upload

---
+++ calc.py
@@ -0,0 +1,14 @@
+def power(a, b):
+    return a ** b
+
+def cubic(x):
+    return x ** 3
+
+def divide(a, b):
+    return a / b
+
+def factorial(n):
+    result = 1
+    for i in range(1, n+1):
+        result *= i
+    return result
---
+++ main.py
@@ -0,0 +1,6 @@
+import calc
+
+print(calc.power(2, 3))     # 2의 3제곱 → 8
+print(calc.cubic(3))        # 3의 세제곱 → 27
+print(calc.divide(10, 2))   # 나누기 → 5.0
+print(calc.factorial(5))    # 팩토리얼 → 120
